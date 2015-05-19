3f94fd9f4141ee69646ec6002680f5718633c72b
kishan bhakta <kishan.d.bhakta@gmail.com>
2015-05-19 16:54:56 -0400

    added some comments

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ $('section').on ('click', function() {
   $('section').removeClass('firstSection');
   $(this).addClass('firstSection')
 });
+
+// This makes my firstSection to be on and other sections to be off.
+// When I click on the 2nd section it removes the class to firstSection and 3rd
+// Same applies to the 3rd section
